8d45687c3210c08c6164b87b4cf7d57f33b7fe78
Tahiya Nuzhath Khan <tnkhanok01@gmail.com>
2026-04-27 04:59:07 +0800

    Delete second.php

--- second.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	
-	session_destroy(); // what if we do not put session_destroy?
-	
-	
-	echo "<br> You are successfully logout! Click here to <a href='login.php'>login again</a>";
-?>
