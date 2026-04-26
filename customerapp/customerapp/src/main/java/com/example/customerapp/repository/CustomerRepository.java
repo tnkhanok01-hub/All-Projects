d87ab5be8954443c48f94ef747a27932be86152d
Tahiya Nuzhath Khan <tnkhanok01@gmail.com>
2026-04-27 04:53:36 +0800

    Delete customerapp/customerapp directory

--- customerapp/customerapp/src/main/java/com/example/customerapp/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.customerapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.customerapp.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
